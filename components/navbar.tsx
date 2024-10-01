9f8ec0bf5d3e22e2d36085a39f45562a1c1e8bd8
MingGH <1558772318@qq.com>
2024-10-01 16:32:42 +0800

    Clean up useless code on the homepage and update some link jumps

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,26 +27,26 @@ import {
 } from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
+  // const searchInput = (
+  //   <Input
+  //     aria-label="Search"
+  //     classNames={{
+  //       inputWrapper: "bg-default-100",
+  //       input: "text-sm",
+  //     }}
+  //     endContent={
+  //       <Kbd className="hidden lg:inline-block" keys={["command"]}>
+  //         K
+  //       </Kbd>
+  //     }
+  //     labelPlacement="outside"
+  //     placeholder="Search..."
+  //     startContent={
+  //       <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+  //     }
+  //     type="search"
+  //   />
+  // );
 
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
@@ -80,28 +80,28 @@ export const Navbar = () => {
         justify="end"
       >
         <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal aria-label="Discord" href={siteConfig.links.discord}>
-            <DiscordIcon className="text-default-500" />
-          </Link>
+          {/*<Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>*/}
+          {/*  <TwitterIcon className="text-default-500" />*/}
+          {/*</Link>*/}
+          {/*<Link isExternal aria-label="Discord" href={siteConfig.links.discord}>*/}
+          {/*  <DiscordIcon className="text-default-500" />*/}
+          {/*</Link>*/}
           <Link isExternal aria-label="Github" href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+        {/*<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>*/}
         <NavbarItem className="hidden md:flex">
           <Button
             isExternal
             as={Link}
             className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
+            href={siteConfig.links.ninja996}
             startContent={<HeartFilledIcon className="text-danger" />}
             variant="flat"
           >
-            Sponsor
+            Jump to 996.ninja
           </Button>
         </NavbarItem>
       </NavbarContent>
@@ -114,28 +114,28 @@ export const Navbar = () => {
         <NavbarMenuToggle />
       </NavbarContent>
 
-      <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
+      {/*<NavbarMenu>*/}
+      {/*  {searchInput}*/}
+      {/*  <div className="mx-4 mt-2 flex flex-col gap-2">*/}
+      {/*    {siteConfig.navMenuItems.map((item, index) => (*/}
+      {/*      <NavbarMenuItem key={`${item}-${index}`}>*/}
+      {/*        <Link*/}
+      {/*          color={*/}
+      {/*            index === 2*/}
+      {/*              ? "primary"*/}
+      {/*              : index === siteConfig.navMenuItems.length - 1*/}
+      {/*                ? "danger"*/}
+      {/*                : "foreground"*/}
+      {/*          }*/}
+      {/*          href="#"*/}
+      {/*          size="lg"*/}
+      {/*        >*/}
+      {/*          {item.label}*/}
+      {/*        </Link>*/}
+      {/*      </NavbarMenuItem>*/}
+      {/*    ))}*/}
+      {/*  </div>*/}
+      {/*</NavbarMenu>*/}
     </NextUINavbar>
   );
 };
